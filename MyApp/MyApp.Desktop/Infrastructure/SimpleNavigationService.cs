7f40e7bfea732950ac7cb12c02b9cb8091c130de
vmilev <vmilev@outlook.com>
2015-04-01 19:49:44 +0300

    forgot some files

---
+++ MyApp/MyApp.Desktop/Infrastructure/SimpleNavigationService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Practices.Prism.Mvvm.Interfaces;
+using System;
+using System.Windows.Navigation;
+
+namespace CCT.Desktop.Infrastructure
+{
+    public class SimpleNavigationService : INavigationService
+    {
+        private NavigationService navigationService;
+
+        public SimpleNavigationService(NavigationService nativeService)
+        {
+            this.navigationService = nativeService;
+        }
+
+        public bool CanGoBack()
+        {
+            return this.navigationService.CanGoBack;
+        }
+
+        public void ClearHistory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GoBack()
+        {
+            this.navigationService.GoBack();
+        }
+
+        public bool Navigate(string pageToken, object parameter)
+        {
+            return this.navigationService.Navigate(new Uri(string.Format("Views/{0}.xaml", pageToken), UriKind.Relative));
+        }
+
+        public void RestoreSavedNavigation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Suspending()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
