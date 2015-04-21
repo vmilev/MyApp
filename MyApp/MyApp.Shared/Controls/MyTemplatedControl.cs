eaaf9c2dbf3bb56cc478b9065bb1f36a9169d08a
vmilev <vmilev@outlook.com>
2015-04-21 15:09:54 +0300

    Added XamlPreprocessor

---
+++ MyApp/MyApp.Shared/Controls/MyTemplatedControl.cs
@@ -0,0 +1,28 @@
+﻿#if WPF
+using System.Windows;
+using System.Windows.Controls;
+#endif
+
+#if NETFX_CORE
+using Windows.UI.Xaml.Controls;
+#endif
+
+namespace MyApp.Controls
+{
+    public sealed class MyTemplatedControl : Control
+    {
+        public MyTemplatedControl()
+        {
+#if NETFX_CORE
+            this.DefaultStyleKey = typeof(MyTemplatedControl);
+#endif
+        }
+
+#if WPF
+        static MyTemplatedControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyTemplatedControl), new FrameworkPropertyMetadata(typeof(MyTemplatedControl)));
+        }
+#endif
+    }
+}
---
+++ XamlPreprocessor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace XamlPreprocessor
+{
+    public class XamlPreprocessor
+    {
+        const string regex = @"""using:(?<namespace>[\w\.\-_]*)""";
+
+        public static void Main(string[] args)
+        {
+            string unprocessedXaml = File.ReadAllText(args[0]);
+
+            //Create destination directory if it doesn't exist
+            (new FileInfo(args[1])).Directory.Create();
+            
+            var preprocessor = new XamlPreprocessor();
+            var processedXaml = preprocessor.Process(unprocessedXaml);
+
+            File.WriteAllText(args[1], processedXaml);
+        }
+
+        public string Process(string text)
+        {
+            return Regex.Replace(text, regex, RegExMatchEvaluator);
+        }
+
+        private string RegExMatchEvaluator(Match match)
+        {
+            return match.Result("\"clr-namespace:" + match.Groups["namespace"] + "\"");
+        }
+    }
+}
